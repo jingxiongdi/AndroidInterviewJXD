7b4645469b0aa65f070b0213ec7f6817da164c15
jingxiongdi <937969716@qq.com>
2020-10-18 14:56:54 +0800

    上传ConstraintLayout学习记录

--- app/src/main/java/com/example/androidinterviewjxd/layout/ConstraintLayoutActivity.java
+++ app/src/main/java/com/example/androidinterviewjxd/layout/ConstraintLayoutActivity.java
@@ -6,6 +6,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.androidinterviewjxd.R;
 
+/**
+ * layout_constraintLeft_toRightOf：可以理解为当前控件的左边，在指定控件的右边
+ *
+ */
 public class ConstraintLayoutActivity extends AppCompatActivity {
 
     @Override
